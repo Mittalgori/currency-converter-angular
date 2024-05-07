51b611a378c72474673baa8135d2571efbaf637a
Mittalgori <mitu796@gmail.com>
2024-05-07 09:50:12 +0530

    firstcommit

---
+++ src/app/currencyconverter.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrencyconverterService {
+
+  private apiUrl = "https://v6.exchangerate-api.com/v6/d0907616d62d687468c515a0/latest/USD"
+  constructor(private http: HttpClient) { }
+
+  getExchangeRates() : Observable<any>{
+    return this.http.get<any>(this.apiUrl).pipe(
+      catchError(this.handleError)
+      // catchError is used to intercept http errors and delegate to handleError
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<any>{
+    console.log("Http error occured : ", error);  // error received is logged
+    return throwError('Something went wrong. Please try again...');
+    // throwError is used to generate userfriendly message
+  }
+}
